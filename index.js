66a033608afde311dc0c7b46e46a9acda2e9f4a8
Shubham Laware <lawareshubham647@gmail.com>
2024-07-07 15:47:48 +0530

    files uploaded

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/account',getBalanceRouter);
